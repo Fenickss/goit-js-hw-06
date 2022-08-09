ad2ec299db893f33da18113535c333a17664ee09
Fenickss <denis.suxov.1991@mail.ru>
2022-08-09 10:39:23 +0300

    task-1

--- task-1.js
+++ task-1.js
@@ -10,3 +10,12 @@
 // console.log(getUserNames(users));
 // // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 // ```
+
+import users from "./users.js";
+
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
+
+console.log(getUserNames(users));
+
