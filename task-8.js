d92340a6608bc72dec04b5309835a2c749d8dee0
Fenickss <denis.suxov.1991@mail.ru>
2022-08-09 16:22:09 +0300

    +

--- task-8.js
+++ task-8.js
@@ -14,9 +14,4 @@ const getUsersWithFriend = (users, friendName) => {
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// const getUsersWithFriend = (users, friendName) =>
-//   users
-//     .filter(user => user.friends.some(friend => friend === friendName))
-//     .map(user => user.name);
-// console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
